a8794d24148d25585682a89e9bae31e754d08bd8
JAR3D <j12rosado@gmail.com>
2019-04-10 20:13:24 +0100

    server minimum ejs

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, '..', '..', 'dist', 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'views'));
+
+app.get('/', (req, res) => {
+    res.render('page1');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listen on port ${PORT}`);
+});
