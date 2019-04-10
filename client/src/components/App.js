17613dd4708c314b96a6d518dfb99083a0e9ebe1
JAR3D <j12rosado@gmail.com>
2019-04-10 19:57:26 +0100

    webpack dev server working minimal

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    state = {
+        someState: 'some state'
+    }
+
+    render() {
+        return (
+            <div>
+                App Component
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client', 'src', 'client.js'),
+  output: {
+    filename: 'client_bundle.js',
+    // output.path needs to be absolute
+    path: path.join(__dirname, 'dist', 'public'),
+    publicPath: '/',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    //   {
+    //     test: /\.css$/,
+    //     use: ['style-loader', 'css-loader'],
+    //   },
+    //   {
+    //     test: /\.html$/,
+    //     use: [
+    //       {
+    //         loader: 'html-loader',
+    //         options: { minimize: false },
+    //       },
+    //     ],
+    //   },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // new HtmlWebPackPlugin({
+    //   template: path.join(__dirname, 'public', 'index.html'),
+    //   filename: './index.html', // defined for the dist folder
+    // }),
+    // new MiniCssExtractPlugin({
+    //   // Options similar to the same options in webpackOptions.output
+    //   // both options are optional
+    //   filename: '[name].css',
+    //   chunkFilename: '[id].css',
+    // }),
+  ],
+};
+
+module.exports = config;
