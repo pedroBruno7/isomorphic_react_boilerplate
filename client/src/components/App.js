47580f5298c62bec1d9ad3ab0c2ee57cb76fdb56
Joao <j12rosado@gmail.com>
2019-04-11 12:03:14 +0100

    ssr com rotas iguais no servidor e cliente na rota users

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -16,17 +16,22 @@ class App extends React.Component {
     }
 
     render() {
+        // throw new Error('coco')
         return (
             <div>
-                App Component
+                <h1>App Component</h1>
+                <h1>ola</h1>
                 <button onClick={this.clickHandle}>Click me</button>
                 <Header/>
                 <Switch>
                     <Route path='/link1' component={Link1}/>
                     <Route path='/link2' component={Link2}/>
-                    <Route path='/users' component={Users}/>
+                    <Route path='/users' render={props => (
+                        <Users {...props} data={this.props.dataFromDB ? this.props.dataFromDB.users : undefined} />
+                    )}/>
                 </Switch>
             </div>
+            
         )
     }
 }
--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,41 @@ import { StaticRouter } from "react-router-dom";
 
 import App from '../../client/src/components/App';
 
+const users ={ 
+  users: [
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "d", age:23},
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "d", age:23},
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "d", age:23},
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "d", age:23},
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "d", age:23},
+      {name: "ze", age:23},
+      {name: "a", age:23},
+      {name: "b", age:23},
+      {name: "c", age:23},
+      {name: "micael", age:44}
+  ]
+};
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -24,42 +59,33 @@ app.get("/", (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-    const users ={ 
-        users: [
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "d", age:23},
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "d", age:23},
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "d", age:23},
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "d", age:23},
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "d", age:23},
-            {name: "ze", age:23},
-            {name: "a", age:23},
-            {name: "b", age:23},
-            {name: "c", age:23},
-            {name: "micael", age:44}
-        ]
-    }
     
       res.send(JSON.stringify(users))
+})
+
+app.get('/users', (req, res) => {
+  const context = {};
+
+  const content = ReactDOMServer.renderToString(
+    <StaticRouter location={req.url} context={context}>
+      <App dataFromDB={ users }/>
+    </StaticRouter>
+  );
+
+  const html = `
+          <html>
+            <head>
+            </head>
+              <body>
+                <div id='root'>
+                  ${content}
+                </div>
+                <script src="./client_bundle.js"></script>
+              </body>
+          </html>
+        `;
+
+  res.send(html);
 })
 
 app.get("*", (req, res) => {
